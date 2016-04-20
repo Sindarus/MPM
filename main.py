9ec4c753dedfe2f38d662b02c4ab267ea995c6c4
TheThoron <m.vilao@laposte.net>
2016-04-20 09:54:26 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+﻿from numpy import matrix
+
+w=int(input("Rentrer la valeur de w : "))
+
+#x = matrix(str(x)+' '+str(dx)+' '+str(y)+' '+str(dy))
+#u = matrix(str(ux)+' '+str(uy))
+
+A = matrix('0 1 0 0; '+str(3*w*w)+' 0 0 '+str(2*w)+'; 0 0 0 1; 0 '+str(-2*w)+' 0 0')
+B = matrix('0 1 0 0; 0 0 0 1')
+
+print (A[3, 1])
+print (A)
+print (B)
